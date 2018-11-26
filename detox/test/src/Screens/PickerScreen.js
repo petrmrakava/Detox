e8aaee7635e52f3cb318ed814cae78af164cdfe4
Petr Mrákava <petr.mrakava@24imedia.com>
2018-11-26 16:54:08 +0100

    Added ios picker example

---
+++ detox/test/src/Screens/PickerScreen.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Picker,
+  PickerItem
+} from 'react-native';
+
+export default class DatePickerScreen extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedValue: "Foo"
+    }
+  }
+
+  render() {
+    return (
+      <View style={{flex: 1, paddingTop: 20, justifyContent: 'center', alignItems: 'center'}}>
+        <Text style = {styles.labelText} testID='FooLabel'>
+        {this.state.selectedValue}
+        </Text>
+        <Picker
+          testID="FooPicker"
+          selectedValue={this.state.selectedValue}
+          onValueChange={(selectedValue) => this.setState({ selectedValue })}
+          style={{ width: '100%', height: 200}}>
+          <PickerItem value="Foo" label="Foo" />
+          <PickerItem value="Bar" label="Bar" />
+          <PickerItem value="Baz" label="Baz" />
+        </Picker>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+    labelText: {
+      textAlign: 'center',
+    }  
+});
